7566d3175be6ec64ff3437a3903a10dc22d0a007
mst <mstolpovskym@gmail.com>
2023-05-31 00:46:46 +0300

    add school mapper

---
+++ src/main/java/ru/itis/yaylunch/dto/response/SchoolResponse.java
@@ -0,0 +1,19 @@
+package ru.itis.yaylunch.dto.response;
+
+import lombok.Data;
+
+@Data
+public class SchoolResponse {
+
+    private String name;
+
+    private String country;
+
+    private String city;
+
+    private String street;
+
+    private String house;
+
+    private String postalCode;
+}
---
+++ src/main/java/ru/itis/yaylunch/mapper/SchoolMapper.java
@@ -0,0 +1,17 @@
+package ru.itis.yaylunch.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import ru.itis.yaylunch.dto.response.SchoolResponse;
+import ru.itis.yaylunch.models.School;
+
+@Mapper(componentModel = "spring")
+public interface SchoolMapper {
+
+    @Mapping(target = "country", source = "address.country")
+    @Mapping(target = "city", source = "address.country")
+    @Mapping(target = "street", source = "address.street")
+    @Mapping(target = "house", source = "address.house")
+    @Mapping(target = "postalCode", source = "address.postalCode")
+    SchoolResponse toResponse(School school);
+}
